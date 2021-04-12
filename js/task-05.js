d544fd03b3268ea409fbf133589bb51be243d50e
Serhii-Nefedov <uvaafestival@gmail.com>
2021-04-12 22:55:06 +0300

    del comments

--- js/task-05.js
+++ js/task-05.js
@@ -12,14 +12,3 @@ function onInputChange(event) {
       nameLabel.textContent = event.currentTarget.value;
     }
 };
-/*
-let input = document.getElementById('name-input');
-let nameOutput = document.getElementById('name-output');
-
-input.oninput = function () {
-  if (input.value === '') {
-    nameOutput.innerHTML = 'незнакомец';
-  } else {
-    nameOutput.innerHTML = input.value;
-  }
-};*/
